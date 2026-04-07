bbcc0b5b3949bb64e48e825c6d45a841d8b5703d
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-07 20:28:51 +0700

    init dan struct

---
+++ structarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct detailAalamat 
+{
+    string desa;
+    string kota;
+
+};
+
+struct Orang
+{
+    string nama;
+    detailAalamat alamat;
+    int umur;
+};
+
