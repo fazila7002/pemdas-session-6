9e39e618fcc24201d71db03c63f422a292f6aa23
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-07 20:06:08 +0700

    main dan input getline

--- structgetline.cpp
+++ structgetline.cpp
@@ -14,3 +14,19 @@ struct Orang
     int umur;
 };
 
+int main()
+{
+    Orang mhs;
+    cout << "Isikan data nama   : ";
+    getline(cin , mhs.nama);
+    cout << "Isikan data desa   : ";
+    getline(cin , mhs.alamat.desa);
+    cout << "isikan data kota   : ";
+    getline(cin , mhs.alamat.kota);
+    cout << "isikan data umur   : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+
+
+}
