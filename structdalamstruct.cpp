55d0008a6a1a65f1d5f31fceaaa84d6a19cf604f
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-07 19:40:43 +0700

    struct

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+
+};
+
