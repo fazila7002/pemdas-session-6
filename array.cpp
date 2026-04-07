712c9ad03b1b4bb796af8ea4ed5f45f608f50fa3
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-07 18:40:48 +0700

    print manual

--- array.cpp
+++ array.cpp
@@ -23,6 +23,14 @@ int main(){
     cout << "isikan data kemlima : ";
     cin >> datamu[4];
     cout << endl;
+
+
+    cout << "data pertamaa = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "datakelima = " << datamu[4] << endl;
+    cout << endl;
     
  
 }
